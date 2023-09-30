bfaab5fabd0ee36247260aa94478253ee040e68a
Valeria Montano <110261804+ValeriaMontano@users.noreply.github.com>
2023-09-30 10:25:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const startEl = document.getElementById("start");
+const stopEl = document.getElementById("stop");
+const resetEl = document.getElementById("reset");
+const timerEl = document.getElementById("timer");
+
+let interval;
+let timeLeft = 1500; //1500 = 25 min
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60); //minutes
+  let seconds = timeLeft % 60; //remainder
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+
+  timerEl.innerHTML = formattedTime;
+}
+function startTimer() {
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      alert("Time`s up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+function resetTimer() {
+  timeLeft = 1500;
+  updateTimer();
+}
+startEl.addEventListener("click", startTimer);
+
+stopEl.addEventListener("click", stopTimer);
+
+resetEl.addEventListener("click", resetTimer);
